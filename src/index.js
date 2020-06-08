cf02e5210b4695be1eca69006e54bb15218618ea
Jack Milton <originalmiltonz@gmail.com>
2020-06-08 14:47:55 +0100

    Inital file set up: Added increment buttons and reset button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App';
+
+import { createStore } from "redux";
+
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PLAYER1": return { ...state, player1: state.player1 + 1 };
+    case "PLAYER2": return { ...state, player2: state.player2 + 1 };
+    case "RESET": return initial;
+    default: return state;
+  }
+};
+
+const store = createStore(
+  reducer,
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+store.dispatch({ type: "PLAYER1" });
+store.dispatch({ type: "PLAYER2" });
+store.dispatch({ type: "RESET" })
+
+const render = () => {
+  let state = store.getState();
+  ReactDOM.render(
+    <App
+      player1={state.player1}
+      handleP1Increment={() => store.dispatch({ type: "PLAYER1" })}
+      player2={state.player2}
+      handleP2Increment={() => store.dispatch({ type: "PLAYER2" })}
+      handleReset={() => store.dispatch({ type: "RESET" })} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
