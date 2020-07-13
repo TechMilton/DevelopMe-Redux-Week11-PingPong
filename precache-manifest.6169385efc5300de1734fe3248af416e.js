98534d106ebc6764b28d65142dedb6bde6eca51e
Jack Milton <originalmiltonz@gmail.com>
2020-07-13 13:46:19 +0100

    Updates

---
+++ precache-manifest.6169385efc5300de1734fe3248af416e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177e803e9cee55d2af885ff9cc2a6812",
+    "url": "/TechMilton.github.io/Ping-Pong-App/index.html"
+  },
+  {
+    "revision": "28d9d526e1136625123e",
+    "url": "/TechMilton.github.io/Ping-Pong-App/static/js/2.8e349b44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TechMilton.github.io/Ping-Pong-App/static/js/2.8e349b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3f3b90a68cc0b4bd64",
+    "url": "/TechMilton.github.io/Ping-Pong-App/static/js/main.8cf11960.chunk.js"
+  },
+  {
+    "revision": "f4744aa6b315c4ff8e7d",
+    "url": "/TechMilton.github.io/Ping-Pong-App/static/js/runtime-main.145d410a.js"
+  }
+]);
